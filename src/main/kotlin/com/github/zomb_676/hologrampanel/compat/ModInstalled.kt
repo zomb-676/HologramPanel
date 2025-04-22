473b25e833801d32a056b8edeee6e816ad5d280a
zomb_676 <zomb_676@outlook.com>
2025-04-22 22:08:04 +0800

    use correct rei mod id

--- src/main/kotlin/com/github/zomb_676/hologrampanel/compat/ModInstalled.kt
+++ src/main/kotlin/com/github/zomb_676/hologrampanel/compat/ModInstalled.kt
@@ -2,7 +2,8 @@ package com.github.zomb_676.hologrampanel.compat
 
 import net.neoforged.fml.ModList
 
+@Suppress("SpellCheckingInspection")
 object ModInstalled {
-    val reiInstalled = ModList.get().isLoaded("rei")
+    val reiInstalled = ModList.get().isLoaded("roughlyenoughitems")
     val jeiInstalled = ModList.get().isLoaded("jei")
 }
