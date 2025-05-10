fd6e826104f2f56d9ce9b912419e4f865bca46b9
zomb_676 <zomb_676@outlook.com>
2025-05-10 21:33:06 +0800

    let CycleSelector exit under some circumstances when keyboard conflicts

--- src/main/kotlin/com/github/zomb_676/hologrampanel/util/selector/CycleSelector.kt
+++ src/main/kotlin/com/github/zomb_676/hologrampanel/util/selector/CycleSelector.kt
@@ -173,6 +173,10 @@ class CycleSelector(topEntry: CycleEntry.Group) : CycleEntry.SelectorCallback {
 
         fun onClick() {
             val selector = this.instance ?: return
+            if (Minecraft.getInstance().screen != null) {
+               this.tryEnd()
+                return
+            }
             if (selector.canBackToParent) {
                 selector.recoveryToParent()
             } else {
